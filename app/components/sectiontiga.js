d3b846efd7295c6525037c8e3df2f0b7975f3770
RikyAdiPutra <rikyadiputra76@gmail.com>
2023-05-19 23:08:00 +0700

    membuat pembungkus untuk elemen dan mendwonload gamabr nya

--- app/components/sectiontiga.js
+++ app/components/sectiontiga.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Sectiontiga() {
      return (
-          <>
-               <h1>Ini Bagian Section Tiga</h1>
-          </>
+          <div className="container">
+               <div className="text">
+                    <div className="text1"></div>
+                    <div className="text2"></div>
+               </div>
+               <div className="image"></div>
+          </div>
      );
 }
