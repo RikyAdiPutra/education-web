1aee1cc96ac4d0842e10d8e6b2b1adf93cd9ee66
Sapto Prasojo <prasojodesigner@gmail.com>
2023-05-19 20:22:45 +0700

    update

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/header'
+import Title from './title'
+
+export default function PageAbout() {
+  return (
+    <div>
+      <Title/>
+      PageAbout
+
+    </div>
+  )
+}
