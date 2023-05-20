8b8c35df409afb26de29e8ceb127f62fba95d6a5
RikyAdiPutra <rikyadiputra76@gmail.com>
2023-05-20 11:53:56 +0700

    memodifikasi content untuk section 3 dan mendwonload 3 foto dari figma sesuai section

--- app/components/sectiontiga.js
+++ app/components/sectiontiga.js
@@ -1,13 +1,24 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Sectiontiga() {
      return (
           <div className="container">
-               <div className="text">
-                    <div className="text1"></div>
-                    <div className="text2"></div>
+               <div className="main">
+                    <div className="section1">
+                         <Image
+                              src="/images/satu.png"
+                              width={475}
+                              height={175}
+                         />
+                    </div>
+                    <div className="section">
+                         <Image src="/images/dua.png" width={132} height={45} />
+                    </div>
                </div>
-               <div className="image"></div>
+               <div className="section3">
+                    <Image src="/images/tiga.png" width={581} height={474} />
+               </div>
           </div>
      );
 }
