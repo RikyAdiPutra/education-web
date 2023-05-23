35943d46cd98edc3e22fc7f98a0367ef06c8d47d
agus susilo <agussusilo0893@gmail.com>
2023-05-23 19:07:31 +0700

    update task sectio satu

--- app/components/header.js
+++ app/components/header.js
@@ -4,26 +4,27 @@ import "./style.css";
 import Link from 'next/link';
 import { FaArrowRight } from "react-icons/fa";
 import { FaAngleDown } from "react-icons/fa";
-
 export default function Header() {
   return (
     <div className="header">
+      <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet"></link>
       <div className="container">
         <div className="left">
-          <Link href="/"><Image src="/images/logo.png" width={130} height={17}/></Link>
+          <Link href="/"><Image src="/images/logo.png" width={130} height={17} /></Link>
           <ul className="mainmenu">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
             <li><Link href="/products">Products</Link></li>
-            <li><Link href="/">Games <FaAngleDown/></Link></li>
+            <li><Link href="/">Games <FaAngleDown /></Link></li>
           </ul>
         </div>
         <div className="right">
-			<Link href="/" className="username">
-				<Image src="/images/alex.png" width={42} height={40} alt="image profile" />
-				Alex
-			</Link>
-			<Link href="/" className="signout">Sign Out <FaArrowRight/></Link>
+          <Link href="/" className="username">
+            <Image src="/images/alex.png" width={42} height={40} alt="image profile" />
+            Alex
+          </Link>
+          <Link href="/" className="signout">Sign Out <FaArrowRight /></Link>
         </div>
       </div>
     </div>
