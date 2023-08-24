95e99463c92bcfc9965294ecb9500d6b17de104d
RikyAdiPutra <rikyadiputra76@gmail.com>
2023-08-24 17:10:24 +0700

    slight changes

--- app/components/sectionempat.js
+++ app/components/sectionempat.js
@@ -1,29 +1,34 @@
 import React from "react";
-import Image from 'next/image'
-import "./style.css"
+import Image from "next/image";
+import "./style.css";
 
 export default function Sectionempat() {
-    return (
-     <section class="sectionempat">
-        <div className="container">
+  return (
+    <section className="sectionempat">
+      <div className="container">
         <div className="row">
-                    <div className="left-second-section">
-                        <Image src="/images/image 1.png" width="490" height="376" alt="alt"></Image>
-                    </div>
-                    </div>
-                    <div className="right-second-section">
-                        <div className="wording-second-section">
-                            <h2>Increase your vocabulary</h2>
-                            <p>Save statistics on your achievements, words learned, and mistakes </p>
-                        </div>
-               
-               <div className="about platform">
-               <Image src="/images/About platform.png" width={132} height={45} />
-                            </div>
-                            </div>
-              
-                         
+          <div className="left-second-section">
+            <Image
+              src="/images/image 1.png"
+              width="680"
+              height="410"
+              alt="alt"
+            ></Image>
           </div>
-          </section>
-    );
+        </div>
+        <div className="right-second-section">
+          <div className="wording-second-section">
+            <h2>Increase your vocabulary</h2>
+            <p>
+              Save statistics on your achievements, words learned, and mistakes{" "}
+            </p>
+          </div>
+
+          <div className="about platform">
+            <Image src="/images/About platform.png" width={88} height={14} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/components/sectiontiga.js
+++ app/components/sectiontiga.js
@@ -4,7 +4,7 @@ import "./style.css"
 
 export default function Sectiontiga() {
      return (
-          <div className="container">
+          <div className="container-tiga">
                <div className="main">
                     <div className="section1">
                          <Image
